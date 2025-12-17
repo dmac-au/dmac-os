68660a403edc23a299bf16fd36b1001f57321f4c
Dan MacLeod <dan@dmac.au>
2025-12-17 21:12:34 +1030

    initial config

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,20 +4,17 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# install basic packages
+dnf5 install -y neovim
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# install graphical packages
+dnf5 install -y gdm mate-polkit xwayland-satellite
+# Install niri from the yalter/niri COPR
+dnf5 -y copr enable yalter/niri
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install niri dms dms-greeter
+dnf5 -y copr disable yalter/niri
+dnf5 -y copr disable avengemedia/dms
 
 #### Example for enabling a System Unit File
 
