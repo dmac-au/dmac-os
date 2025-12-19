13456809225b9f056c4a29272061bc3f4346ea46
Dan MacLeod <dan@dmac.au>
2025-12-20 01:06:48 +1030

    add fish shell, add intune

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,19 +5,23 @@ set -ouex pipefail
 ### Install packages
 
 # install basic packages
-dnf5 install -y neovim
+dnf5 install -y neovim podman distrobox fish
 
 # install graphical packages
 dnf5 install -y mate-polkit xwayland-satellite kitty
+
 # Install niri from the yalter/niri COPR
 dnf5 -y copr enable yalter/niri
 dnf5 -y copr enable avengemedia/dms
+
 dnf5 -y install niri dms dms-greeter
+
+# disable coprs
 dnf5 -y copr disable yalter/niri
 dnf5 -y copr disable avengemedia/dms
 
-# dms greeter enable
+# install intune
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf5 config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod
+sudo dnf5 install intune-portal
