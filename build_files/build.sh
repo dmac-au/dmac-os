c0a479cc859382a4f6e0d1c768ffe31e2977a029
Dan MacLeod <dan@dmac.au>
2025-12-21 00:14:48 +1030

    trying to install intune

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 dnf5 install -y neovim podman distrobox fish
 
 # install graphical packages
-dnf5 install -y mate-polkit xwayland-satellite kitty
+dnf5 install -y mate-polkit xwayland-satellite kitty nemo
 
 # Install niri from the yalter/niri COPR
 dnf5 -y copr enable yalter/niri
@@ -22,6 +22,13 @@ dnf5 -y copr disable avengemedia/dms
 
 # install intune
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo dnf5 config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod
-sudo dnf5 install intune-portal
+# install prerequisites
+dnf5 -y install webkit2gtk4.1 gnome-keyring
+
+# fuck with webkit2gtk4
+ln -s /usr/lib64/libwebkit2gtk-4.1.so.37 /usr/lib64/libwebkit2gtk-4.0.so.37
+
+# actually install it
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf5 config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod/config.repo
+dnf5 -y install intune-portal
