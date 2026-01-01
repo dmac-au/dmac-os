1c85d7a69cf6b95cd485e69eaa4c3aaf3e1377db
Dan MacLeod <dan@dmac.au>
2026-01-01 22:57:41 +1030

    remove intune until it's ready

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ set -ouex pipefail
 dnf5 install -y neovim podman distrobox fish
 
 # install graphical packages
-dnf5 install -y mate-polkit xwayland-satellite kitty nemo
+dnf5 install -y mate-polkit xwayland-satellite kitty nemo gnome-keyring pavucontrol
 
 # Install niri from the yalter/niri COPR
 dnf5 -y copr enable yalter/niri
@@ -19,16 +19,3 @@ dnf5 -y install niri dms dms-greeter
 # disable coprs
 dnf5 -y copr disable yalter/niri
 dnf5 -y copr disable avengemedia/dms
-
-# install intune
-
-# install prerequisites
-dnf5 -y install webkit2gtk4.1 gnome-keyring
-
-# fuck with webkit2gtk4
-ln -s /usr/lib64/libwebkit2gtk-4.1.so.37 /usr/lib64/libwebkit2gtk-4.0.so.37
-
-# actually install it
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-dnf5 config-manager addrepo --from-repofile https://packages.microsoft.com/yumrepos/microsoft-rhel9.0-prod/config.repo
-dnf5 -y install intune-portal
